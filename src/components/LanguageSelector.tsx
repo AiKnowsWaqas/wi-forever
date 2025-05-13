af42b07a618f920bc2e5fc8244bb5af6089972b2
AiKnowsWaqas <rouzsaif@gmail.com>
2025-05-14 02:31:26 +0530

    Fix Audio Player Position

--- src/components/LanguageSelector.tsx
+++ src/components/LanguageSelector.tsx
@@ -13,7 +13,7 @@ const LanguageSelector: React.FC = () => {
   ];
 
   return (
-    <div className="fixed top-4 right-4 z-50">
+    <div className="fixed top-4 left-4 z-50">
       <div className="bg-white rounded-lg shadow-md border border-gold-light p-2">
         <div className="flex gap-2">
           {languages.map((lang) => (
