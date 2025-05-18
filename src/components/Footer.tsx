f702329c2244a25095f33981aa7538e736c48afd
AiKnowsWaqas <rouzsaif@gmail.com>
2025-05-18 20:33:45 +0530

    Updated Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -54,7 +54,7 @@ const Footer: React.FC = () => {
       rel="noopener noreferrer"
       className="text-gray-200"
     >
-      © Made with ❤️ (and some tea ☕) by.... 
+      © Made with ❤️ (and some tea ☕) by nsmdwaqas.... 
     </a>
     
       
